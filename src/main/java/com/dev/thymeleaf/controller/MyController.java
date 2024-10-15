94664fd32d231a2dd76d24c0c6aa28ed3f540741
Dumitru Vivat <kaibaman94@gmail.com>
2024-10-15 20:54:35 +0300

    add thymeleaf

---
+++ src/main/java/com/dev/thymeleaf/controller/MyController.java
@@ -0,0 +1,15 @@
+package com.dev.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+
+    @GetMapping("/hello")
+    public String helloWorld(Model model) {
+        model.addAttribute("today", java.time.LocalDate.now());
+        return "helloworld";
+    }
+}
